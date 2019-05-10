2dd60f00d0fd262ee7a97033b3bc5f9bfe72f8ea
Eric Dennison <ericd@netdenizen.com>
2019-05-09 20:53:59 -0400

    Updated from Brython Server: 5/9/2019 8:53:59 PM

--- sierpinsky.py
+++ sierpinsky.py
@@ -1,1 +1,26 @@
 # Sierpinsky demo
+from ggame import CircleAsset, Sprite, App, Color, LineAsset
+
+GASKET_SIZE = 256
+black = Color(0x000000, 1.0)
+thinline = LineStyle(1, black)
+asset = CircleAsset(0.5, thinline, black)
+
+# Draw a Sierpinsky Gasket of width and height d,
+# with upper left at (x, y).
+def draw_sierpinsky(x, y, d):
+    if d <= 1:
+        Sprite(asset, (x, y))                 # base case
+    else:
+        # recurse on upper left quadrant
+        draw_sierpinsky(x, y, d/2)
+        # recurse on upper right quadrant
+        draw_sierpinsky(x + d/2, y, d/2)
+        # recurse on lower right quadrant
+        draw_sierpinsky(x + d/2, y + d/2, d/2)
+
+
+def main():
+    draw_sierpinsky(0, 0, GASKET_SIZE)
+
+App().run()
